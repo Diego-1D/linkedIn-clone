7ff7c4076d0ccff8513662b583883f3817916c21
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-12-15 23:32:31 -0300

    Chamada aos componentes Header, Sidebar e Feed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Feed from './Feed';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      Hello Word!
+    <div className="app">
+      <Header />
+      <div className='app__body'>
+        <Sidebar />
+        <Feed />
+      </div>
     </div>
   );
 }
