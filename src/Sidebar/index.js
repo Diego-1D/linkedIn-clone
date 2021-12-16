50fd8909c5070dde50edea65cd66888bf0df5b62
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-12-15 23:26:52 -0300

    Criação do componente Sidebar

---
+++ src/Sidebar/index.js
@@ -0,0 +1,44 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './styles.css';
+
+const Sidebar = () => {
+
+    const recentItem = (topic) => (
+        <div className='sidebar__recentItem'>
+            <span className='sidebar__hash'>#</span>
+            <p>{topic}</p>
+        </div>
+    );
+
+    return (
+        <div className='sidebar'>
+            <div className='sidebar__top'>
+                <img src='https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDBBMHxleHBsb3JlLWZlZWR8MXx8fGVufDBBfHw%3D&w=1000&q=80' alt='' />
+                <Avatar className='sidebar__avatar' />
+                <h2>Diego Fernandes</h2>
+                <h4>diego1d_fernandes@outlook.com</h4>
+            </div>
+            <div className='sidebar__stats'>
+                <div className='sidebar__stat'>
+                    <p>Who visited you</p>
+                    <p className='sidebar__statNumber'>1,523</p>
+                </div>
+                <div className='sidebar__stat'>
+                    <p>Views on post</p>
+                    <p className='sidebar__statNumber'>2,719</p>
+                </div>
+            </div>
+            <div className='sidebar__bottom'>
+                <p>Recente</p>
+                {recentItem('reactjs')}
+                {recentItem('programming')}
+                {recentItem('softwareengineering')}
+                {recentItem('design')}
+                {recentItem('developer')}
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
