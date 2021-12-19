7cd916210b4b98633c1fd329ab6fec286df89347
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-12-18 23:48:41 -0300

    Integração com o firebase

--- src/Feed/index.js
+++ src/Feed/index.js
@@ -3,30 +3,38 @@ import "./styles.css";
 import { CalendarViewDay, Create, EventNote, Image, Subscriptions } from '@material-ui/icons'
 import InputOptions from '../InputOptions';
 import Post from '../Post';
-import { db } from '../Services/firebase';
+import { db } from '../firebase';
+import firebase from 'firebase';
+import {selectUser} from '../features/userSlice';
+import { useSelector } from 'react-redux';
 
 const Feed = () => {
+    const user = useSelector(selectUser);
     const [input, setInput] = useState('');
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        db.collection('posts').onSnapshot((snapshot) =>
-            setPosts(
-                snapshot.docs.map((doc) => ({
-                    id: doc.id,
-                    data: doc.data(),
-                }))
+        db.collection('posts')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot((snapshot) =>
+                setPosts(
+                    snapshot.docs.map((doc) => ({
+                        id: doc.id,
+                        data: doc.data(),
+                    }))
+                )
             )
-        )
     }, [])
 
     const sendPost = (event) => {
-        event.preventeDefault();
-
+        //Pesquisar sobre essa função
+        event.preventDefault();
         db.collection('posts').add({
-            name: 'Diego Fernandes',
-            description: 'This is a test',
-            message
+            name: user.displayName,
+            description: user.email,
+            message: input,
+            photoUrl: user.photoUrl || '',
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         })
     }
 
@@ -36,7 +44,7 @@ const Feed = () => {
                 <div className='feed__input'>
                     <Create />
                     <form>
-                        <input type='text' />
+                        <input value={input} onChange={e => setInput(e.target.value)} type='text' />
                         <button onClick={sendPost} ontype='submit'>Send</button>
                     </form>
                 </div>
@@ -47,11 +55,16 @@ const Feed = () => {
                     <InputOptions Icon={CalendarViewDay} title='Write article' color='#7FC15E' />
                 </div>
             </div>
-            <Post
-                name='Diego Fernandes'
-                description='This is a test'
-                message='WOW this worked'
-            />
+            {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+                < Post
+                    key={id}
+                    name={name}
+                    description={description}
+                    message={message}
+                    photoUrl={photoUrl}
+                />
+
+            ))}
         </div>
     )
 }
