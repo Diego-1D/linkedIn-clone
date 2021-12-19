9c6e3881e89c90573335384d0431cbe5dff87c74
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-12-18 23:46:08 -0300

    Integração com o firebase

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -1,8 +1,12 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 import './styles.css';
 
 const Sidebar = () => {
+
+    const user = useSelector(selectUser);
 
     const recentItem = (topic) => (
         <div className='sidebar__recentItem'>
@@ -15,9 +19,11 @@ const Sidebar = () => {
         <div className='sidebar'>
             <div className='sidebar__top'>
                 <img src='https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDBBMHxleHBsb3JlLWZlZWR8MXx8fGVufDBBfHw%3D&w=1000&q=80' alt='' />
-                <Avatar className='sidebar__avatar' />
-                <h2>Diego Fernandes</h2>
-                <h4>diego1d_fernandes@outlook.com</h4>
+                <Avatar src={user.photoUrl} className='sidebar__avatar' >
+                    {user.email[0]}
+                </Avatar>
+                <h2>{user.displayName}</h2>
+                <h4> {user.email}</h4>
             </div>
             <div className='sidebar__stats'>
                 <div className='sidebar__stat'>
