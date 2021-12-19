7a320caf3cca4c04922c63114480cae65d58be56
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-12-18 23:47:52 -0300

    Chamada ao firebase para realizar o LogoutOfApp

--- src/Header/index.js
+++ src/Header/index.js
@@ -7,25 +7,42 @@ import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import ChatIcon from '@material-ui/icons/Chat';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import { LinkedIn } from '@material-ui/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from '../features/userSlice';
+import { auth } from '../firebase';
+
 
 const Header = () => {
+
+    const user = useSelector(selectUser);
+
+    const dispatch = useDispatch();
+
+    const logoutOfApp = () => {
+        dispatch(logout())
+        auth.signOut();
+    }
+
     return (
         <div className="header">
             <div className="header__left">
-                <img src='https://cdn-icons-png.flaticon.com/512/174/174857.png'/>
+                <img src='https://cdn-icons-png.flaticon.com/512/174/174857.png' />
                 <div className="header__search">
                     <SearchIcon />
-                    <input type="text" />
+                    <input placeholder='Search' type="text" />
                 </div>
             </div>
             <div className="header__right">
-                <HeaderOptions Icon={HomeIcon} title='Home'/>
-                <HeaderOptions Icon={SupervisorAccountIcon} title='My Network'/>
-                <HeaderOptions Icon={BusinessCenterIcon} title='Jobs'/>
-                <HeaderOptions Icon={ChatIcon} title='Messaging'/>
-                <HeaderOptions Icon={NotificationsIcon} title='Notifications'/>
-                <HeaderOptions avatar='https://avatars.githubusercontent.com/u/50786415?v=4' title='me'/>
+                <HeaderOptions Icon={HomeIcon} title='Home' />
+                <HeaderOptions Icon={SupervisorAccountIcon} title='My Network' />
+                <HeaderOptions Icon={BusinessCenterIcon} title='Jobs' />
+                <HeaderOptions Icon={ChatIcon} title='Messaging' />
+                <HeaderOptions Icon={NotificationsIcon} title='Notifications' />
+                <HeaderOptions
+                    avatar={true}
+                    title='me'
+                    onClick={logoutOfApp}
+                />
             </div>
         </div>
     )
