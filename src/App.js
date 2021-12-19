4e67ae01887a2c65a7b3bf759ff4dd86dbb979aa
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-12-18 23:51:01 -0300

    Integração ao firabse (Login)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import Login from './components/Login';
+import { login, logout, selectUser } from './features/userSlice';
 import Feed from './Feed';
+import { auth } from './firebase';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 function App() {
+
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoURL: userAuth.photoURL,
+        })
+        );
+      } else {
+        dispatch(logout());
+      }
+    })
+  }, []);
   return (
     <div className="app">
       <Header />
-      <div className='app__body'>
-        <Sidebar />
-        <Feed />
-      </div>
+      {!user ?
+        <Login /> : (
+          <div className='app__body'>
+            <Sidebar />
+            <Feed />
+          </div>
+        )}
     </div>
   );
 }
