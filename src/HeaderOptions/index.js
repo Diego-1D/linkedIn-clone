600c1bb3bf8f18abff0cddc1e3c4d32843b1cdd8
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-12-18 23:46:26 -0300

    Integração com o firebase

--- src/HeaderOptions/index.js
+++ src/HeaderOptions/index.js
@@ -1,12 +1,21 @@
 import { Avatar } from '@material-ui/core';
 import './styles.css';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 
-const HeaderOptions = ({avatar, Icon, title}) => {
+const HeaderOptions = ({ avatar, Icon, title, onClick }) => {
+
+    const user = useSelector(selectUser);
+
     return (
-        <div className='headerOption'>
-            {Icon && <Icon className='headerOption__icon'/> }
-            {avatar && <Avatar className='headerOption__icon' src={avatar}/>}
+        <div onClick={onClick} className='headerOption'>
+            {Icon && <Icon className='headerOption__icon' />}
+            {avatar && (
+                <Avatar className='headerOption__icon'>
+                    {user?.email[0]}
+                </Avatar>
+            )}
             <h3 className='headerOption__title'>{title}</h3>
         </div>
     )
