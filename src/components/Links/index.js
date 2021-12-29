40eae5ee6094e1c77a2a771fb8c5a147d7073636
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-12-28 23:41:02 -0300

    Criação do componente Links

---
+++ src/components/Links/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './styles.css';
+
+const Links = () => {
+    return (
+        <div className='container_links'>
+            <div className='area_links'>
+                <div className='area_img'>
+                    <img src='https://cdn.worldvectorlogo.com/logos/linkedin-logo-2013-1.svg' alt='' />
+                </div>
+                <div className='list_area'>
+                    <div className='list'>
+                        <h3>Geral</h3>
+                        <div className='list_item'></div>
+                        <div className='list_item'>Cadastre-se</div>
+                        <div className='list_item'>Central de Ajuda</div>
+                        <div className='list_item'>Sobre</div>
+                        <div className='list_item'>Imprensa</div>
+                        <div className='list_item'>Blog</div>
+                        <div className='list_item'>Carreiras</div>
+                        <div className='list_item'>Programadore</div>
+                    </div>
+                    <div className='list'>
+                        <h3>Navegar pelo LinkedIn</h3>
+                        <div className='list_item'></div>
+                        <div className='list_item'>Learning</div>
+                        <div className='list_item'>Vagas</div>
+                        <div className='list_item'>Salário</div>
+                        <div className='list_item'>Aplicativos</div>
+                        <div className='list_item'>Serviços</div>
+                        <div className='list_item'>Soluções</div>
+                    </div>
+                    <div className='list'>
+                        <h3>Soluções Business</h3>
+                        <div className='list_item'>Talentos</div>
+                        <div className='list_item'>Marketing</div>
+                        <div className='list_item'>Vendas</div>
+                        <div className='list_item'>Learning</div>
+                    </div>
+                    <div className='list'>
+                        <h3>Diretórios</h3>
+                        <div className='list_item'>Cadastre-se</div>
+                        <div className='list_item'>Vagas</div>
+                        <div className='list_item'>Empresas</div>
+                        <div className='list_item'>Salários</div>
+                        <div className='list_item'>Em destaque</div>
+                        <div className='list_item'>Learning</div>
+                        <div className='list_item'>Publicações</div>
+                        <div className='list_item'>Artigos</div>
+                        <div className='list_item'>Instituições de ensino</div>
+                        <div className='list_item'>Notícias</div>
+                        <div className='list_item'>Newsletters</div>
+                        <div className='list_item'>Serviços</div>
+                        <div className='list_item'>Preparação para entrevistas</div>
+                        <div className='list_item'>Soluções</div>
+                        <div className='list_item'>Tópicos de conteúdos</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Links;
