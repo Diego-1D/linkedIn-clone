e76ea4c116fd3b45b0c079d37b8135de4d6e609e
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-12-28 23:40:31 -0300

    Estilização do componente Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,57 +4,21 @@ import './styles.css';
 const Footer = () => {
     return (
         <div className='container_footer'>
-            <div className='area_footer'>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/2560px-LinkedIn_Logo.svg.png' alt='' />
-                <div className='list_area'>
-                    <div className='list'>
-                        <h3>Geral</h3>
-                        <div className='list_item'></div>
-                        <div className='list_item'>Cadastre-se</div>
-                        <div className='list_item'>Central de Ajuda</div>
-                        <div className='list_item'>Sobre</div>
-                        <div className='list_item'>Imprensa</div>
-                        <div className='list_item'>Blog</div>
-                        <div className='list_item'>Carreiras</div>
-                        <div className='list_item'>Programadore</div>
-                    </div>
-                    <div className='list'>
-                        <h3>Navegar pelo LinkedIn</h3>
-                        <div className='list_item'></div>
-                        <div className='list_item'>Learning</div>
-                        <div className='list_item'>Vagas</div>
-                        <div className='list_item'>Salário</div>
-                        <div className='list_item'>Aplicativos</div>
-                        <div className='list_item'>Serviços</div>
-                        <div className='list_item'>Soluções</div>
-                    </div>
-                    <div className='list'>
-                        <h3>Soluções Business</h3>
-                        <div className='list_item'>Talentos</div>
-                        <div className='list_item'>Marketing</div>
-                        <div className='list_item'>Vendas</div>
-                        <div className='list_item'>Learning</div>
-                    </div>
-                    <div className='list'>
-                        <h3>Diretórios</h3>
-                        <div className='list_item'>Cadastre-se</div>
-                        <div className='list_item'>Vagas</div>
-                        <div className='list_item'>Empresas</div>
-                        <div className='list_item'>Salários</div>
-                        <div className='list_item'>Em destaque</div>
-                        <div className='list_item'>Learning</div>
-                        <div className='list_item'>Publicações</div>
-                        <div className='list_item'>Artigos</div>
-                        <div className='list_item'>Instituições de ensino</div>
-                        <div className='list_item'>Notícias</div>
-                        <div className='list_item'>Newsletters</div>
-                        <div className='list_item'>Serviços</div>
-                        <div className='list_item'>Preparação para entrevistas</div>
-                        <div className='list_item'>Soluções</div>
-                        <div className='list_item'>Tópicos de conteúdos</div>
-                    </div>
-                </div>
-            </div>
+            <ul className='wrapper_list'>
+                <li className='list_item_footer'>
+                    <img src='https://cdn.worldvectorlogo.com/logos/linkedin.svg' alt='' />
+                    <span>© 2021</span>
+                </li>
+                <li className='list_item_footer'>Sobre</li>
+                <li className='list_item_footer'>Acessibilidade</li>
+                <li className='list_item_footer'>Contrato do Usuário</li>
+                <li className='list_item_footer'>Política de Privacidade do LinkedIn</li>
+                <li className='list_item_footer'>Política de Cookies</li>
+                <li className='list_item_footer'>Política de Direitos Autorais</li>
+                <li className='list_item_footer'>Política da Marca</li>
+                <li className='list_item_footer'>Controles de visitantes</li>
+                <li className='list_item_footer'>Diretrizes da Comunidade</li>
+            </ul>
         </div>
     )
 }
