ceb1d2e38a09fe112fc0b711d399f03b41f67823
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-12-31 20:18:37 -0300

    Função para exibir o Sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,6 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react';
+import { KeyboardArrowDownOutlined } from '@material-ui/icons';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../features/userSlice';
 import './styles.css';
@@ -8,12 +9,33 @@ const Sidebar = () => {
 
     const user = useSelector(selectUser);
 
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(null);
+
+
     const recentItem = (topic) => (
         <div className='sidebar__recentItem'>
             <span className='sidebar__hash'>#</span>
             <p>{topic}</p>
         </div>
     );
+
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (screenSize < 768) {
+            setActiveMenu(false)
+        } else {
+            setActiveMenu(true);
+        }
+    }, [screenSize]);
 
     return (
         <div className='sidebar'>
@@ -25,23 +47,38 @@ const Sidebar = () => {
                 <h2>{user.displayName}</h2>
                 <h4> {user.email}</h4>
             </div>
-            <div className='sidebar__stats'>
-                <div className='sidebar__stat'>
-                    <p>Conexões</p>
-                    <p className='sidebar__statNumber'>1,523</p>
+
+            {activeMenu && (
+                <div>
+                    <div className='sidebar__stats'>
+                        <div className='sidebar__stat'>
+                            <p>Conexões</p>
+                            <p className='sidebar__statNumber'>1,523</p>
+                        </div>
+                        <div className='sidebar__stat'>
+                            <p>Quem viu seu perfil</p>
+                            <p className='sidebar__statNumber'>2,719</p>
+                        </div>
+                    </div>
+                    <div className='sidebar__bottom'>
+                        <p>Recentes</p>
+                        {recentItem('reactjs')}
+                        {recentItem('programming')}
+                        {recentItem('softwareengineering')}
+                        {recentItem('design')}
+                        {recentItem('developer')}
+                    </div>
                 </div>
-                <div className='sidebar__stat'>
-                    <p>Quem viu seu perfil</p>
-                    <p className='sidebar__statNumber'>2,719</p>
+            )}
+            <div className='view_button_area'>
+                <div className='view_button' onClick={() => setActiveMenu(!activeMenu)}>
+                    {!activeMenu ?
+                        <p>Exibir mais</p>
+                        :
+                        <p>Exibir menos</p>
+                    }
+                    <KeyboardArrowDownOutlined />
                 </div>
-            </div>
-            <div className='sidebar__bottom'>
-                <p>Recentes</p>
-                {recentItem('reactjs')}
-                {recentItem('programming')}
-                {recentItem('softwareengineering')}
-                {recentItem('design')}
-                {recentItem('developer')}
             </div>
         </div>
     )
