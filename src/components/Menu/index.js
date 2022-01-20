1adb29d30ff7e50a63a9283c7e810aef76c91246
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-01-19 23:11:56 -0300

    Criação do menu responsivel

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.css';
+import HeaderOptions from '../HeaderOptions';
+import { BusinessCenter, Chat, Notifications,SupervisorAccount, Home } from '@material-ui/icons';
+const Menu = () => {
+    return (
+        <div className='container_menu'>
+              <HeaderOptions Icon={Home} title='Início' />
+                <HeaderOptions Icon={SupervisorAccount} title='Minha rede' />
+                <HeaderOptions Icon={BusinessCenter} title='Vagas' />
+                <HeaderOptions Icon={Chat} title='Messagens' />
+                <HeaderOptions Icon={Notifications} title='Notificações' />
+                <HeaderOptions
+                    avatar={true}
+                    title='Eu'
+                />
+        </div>
+    )
+}
+
+export default Menu;
